d362cd3058f628df504e36b4667fd33775d9df7d
Jakub Sitnicki <jkbs@frog.home>
2018-05-02 22:29:11 +0200

    TGPL: 'fetch' that uses io.Copy()

---
+++ tgpl/ch01/fetch2/fetch.go
@@ -0,0 +1,28 @@
+// Fetch displays the content found at a given URL
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		output := bufio.NewWriter(os.Stdout)
+		_, err = io.Copy(output, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+        output.Flush()
+	}
+}
